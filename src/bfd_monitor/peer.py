3f07285e4e73ef32c6fe5b909d68a80cdc4b10ce
Viktor Kleen <viktor@kleen.org>
2022-03-04 11:28:04 +0000

    Use Disable/Enable instead of Reset

--- src/bfd_monitor/peer.py
+++ src/bfd_monitor/peer.py
@@ -8,8 +8,6 @@ from gobgp import gobgp_pb2_grpc
 
 from google.protobuf.internal import enum_type_wrapper
 
-GoBGPSessionState = enum_type_wrapper.EnumTypeWrapper(gobgp_pb2._PEERSTATE_SESSIONSTATE)
-
 async def softreset(name):
     logging.info(f"Resetting {name}")
     async with aio.insecure_channel('unix:///run/gobgpd/gobgpd.sock') as channel:
@@ -17,7 +15,7 @@ async def softreset(name):
         addr = None 
         async for resp in stub.ListPeer(gobgp_pb2.ListPeerRequest(address = name, enableAdvertised = False)):
             addr = resp.peer.state.neighbor_address
-            logging.info(f"{name}: {resp.peer.state.neighbor_address}")
+            logging.debug(f"{name}: {resp.peer.state.neighbor_address}")
         if addr is None:
             logging.error(f"{name}: Could not determine peer address")
             return
@@ -26,4 +24,35 @@ async def softreset(name):
             communication = "",
             soft = False,
             direction = gobgp_pb2.ResetPeerRequest.SoftResetDirection.BOTH
+            ))
+
+async def disable(name):
+    logging.info(f"Disabling {name}")
+    async with aio.insecure_channel('unix:///run/gobgpd/gobgpd.sock') as channel:
+        stub = gobgp_pb2_grpc.GobgpApiStub(channel)
+        addr = None 
+        async for resp in stub.ListPeer(gobgp_pb2.ListPeerRequest(address = name, enableAdvertised = False)):
+            addr = resp.peer.state.neighbor_address
+            logging.debug(f"{name}: {resp.peer.state.neighbor_address}")
+        if addr is None:
+            logging.error(f"{name}: Could not determine peer address")
+            return
+        await stub.DisablePeer(gobgp_pb2.DisablePeerRequest(
+            address = addr,
+            communication = "",
+            ))
+
+async def enable(name):
+    logging.info(f"Enabling {name}")
+    async with aio.insecure_channel('unix:///run/gobgpd/gobgpd.sock') as channel:
+        stub = gobgp_pb2_grpc.GobgpApiStub(channel)
+        addr = None 
+        async for resp in stub.ListPeer(gobgp_pb2.ListPeerRequest(address = name, enableAdvertised = False)):
+            addr = resp.peer.state.neighbor_address
+            logging.debug(f"{name}: {resp.peer.state.neighbor_address}")
+        if addr is None:
+            logging.error(f"{name}: Could not determine peer address")
+            return
+        await stub.EnablePeer(gobgp_pb2.EnablePeerRequest(
+            address = addr,
             ))
